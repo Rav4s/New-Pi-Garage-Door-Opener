c9b69ff843a8a11e7daefa5565b6b8a126e91a8f
rav4s <ravidhruvishshah@gmail.com>
2021-04-22 08:38:22 -0500

    Added installation of pip

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
     echo "Updating apt package data..."
     sudo apt-get update
-    echo "Installing python3, rpi-gpio, and git..."
-    sudo apt-get -y install python3 python3-dev python3-rpi.gpio git-all
+    echo "Installing python3, rpi-gpio, pip, and git..."
+    sudo apt-get -y install python3 python3-dev python3-rpi.gpio python3-pip git-all
     echo "Installing Flask..."
     pip3 install flask
     echo "Making directory..."
